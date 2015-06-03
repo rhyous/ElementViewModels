b678bde739ddbaabb4edaac3039f4d6a81c0e41b
Jared A Barneck <rhyous@yahoo.com>
2015-06-03 14:48:48 -0600

    Create ButtonViewModel.js

---
+++ ButtonViewModel.js
@@ -0,0 +1,34 @@
+var ButtonViewModel = function(text, clickMethod, canClickMethod, autoEnableButtonAfterOnClick) {
+  // Private
+  var _self = this;
+  BaseElement(_self);
+  var _text = text;
+  var _clickMethod = clickMethod;
+  var _canClickMethod = canClickMethod;
+  var _autoEnableButtonAfterOnClick = (autoEnableButtonAfterOnClick) && autoEnableButtonAfterOnClick;
+  
+  _self.init = function(obj) {
+
+    // Public
+    obj.text = ko.observable(_text);
+    obj.autoEnableButtonAfterOnClick = ko.observable(_autoEnableButtonAfterOnClick);
+    obj.onClick = (_clickMethod) ? function() {
+      if (!obj.canClick())
+        return;
+      obj.isRunning(true);
+      _clickMethod();
+      if (obj.autoEnableButtonAfterOnClick())
+        obj.isRunning(false);
+    } : function() {
+      return true;
+    };
+
+    obj.isRunning = ko.observable(false);
+
+
+    obj.canClick = ko.computed(function() {
+      return !obj.isRunning() && (!(_canClickMethod) || _canClickMethod());
+    });
+    obj.class = ko.observable();
+  }
+};
