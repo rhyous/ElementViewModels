482ec085a2ce4dd9a7f73dc56ad7bb49a2092260
rhyous <rhyous@yahoo.com>
2015-06-05 17:27:42 -0600

    Added Example pages for AuthenticationViewModel.

--- scripts/CompoundViewModels/AuthenticationViewModel.js
+++ scripts/CompoundViewModels/AuthenticationViewModel.js
@@ -4,8 +4,12 @@
 //  knockout.enterkey.js - needed if the html control this bind to is not inside a form and you
 //                         want the enter key to work when the textbox has focus
 //  knockout.placeholder.js - needed if you want to bind the placeholder with easy syntax
+//
+// Note: Supports loginMethod, beforelogin, and for ajax supports 
 var AuthenticationViewModel = AuthenticationViewModel = function (loginMethod, beforeLogin, onSuccess, onFailure, onError) {
     var _self = this;
+    var defaultUserPlaceholder = "Please enter your username . . .";
+    var defaultPasswordPlaceholder = "Please enter your password . . .";
     var isNullOrWhitespace = function (str) {
         return !str || str.replace(/\s/g, '').length < 1;
     };
@@ -27,9 +31,9 @@ var AuthenticationViewModel = AuthenticationViewModel = function (loginMethod, b
         onError(errMsg);
         _self.submitButton.isRunning(false);
     } : _self.onFailure; // If onError is null, use onFailure
-
-    _self.user = new TextBoxViewModel(new TextBoxModel("", "Please enter your username"));
-    _self.password = new TextBoxViewModel(new TextBoxModel("", "Please enter your password"));
+    
+    _self.user = new TextBoxViewModel(new TextBoxModel("", defaultUserPlaceholder));
+    _self.password = new TextBoxViewModel(new TextBoxModel("", defaultPasswordPlaceholder));
 
     var _loginButtonCanClick = ko.computed(function () {
         return _self.user !== null && !isNullOrWhitespace(_self.user.text()) && _self.password != null && !isNullOrWhitespace(_self.password.text());
@@ -38,7 +42,7 @@ var AuthenticationViewModel = AuthenticationViewModel = function (loginMethod, b
     var _loginButtonClick = function () {
         if (!_loginButtonCanClick()) { return; }
         _self.beforeLogin();
-        _loginMethod(JSON.stringify({ "User": _self.user.text(), "Password": _self.password.text() }), _self.onSuccess, _self.onFailure, _self.onError);
+        _loginMethod({ "User": _self.user.text(), "Password": _self.password.text() }, _self.onSuccess, _self.onFailure, _self.onError);
     };
     _self.submitButton = new ButtonViewModel(new ButtonModel("Login", _loginButtonClick, _loginButtonCanClick, false));
 }
---
+++ tests/CompoundVIewModel/AuthenticationViewModelTests.js
@@ -0,0 +1,9 @@
+QUnit.module("AuthenticationViewModel inherits base element", {
+  beforeEach: function() {
+    vm = new AuthenticationViewModel();
+  }
+});
+
+QUnit.test("Test 1", function(assert) {
+  assert.ok(true);
+});
