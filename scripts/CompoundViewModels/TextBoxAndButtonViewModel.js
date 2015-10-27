3ff5f80c2f23ff562409d36e7f9a314568aa7a61
rhyous <rhyous@yahoo.com>
2015-10-27 11:39:02 -0600

    Added custom validation ability

--- scripts/CompoundViewModels/TextBoxAndButtonViewModel.js
+++ scripts/CompoundViewModels/TextBoxAndButtonViewModel.js
@@ -5,16 +5,17 @@
 //                         want the enter key to work when the textbox has focus
 //  knockout.placeholder.js - needed if you want to bind the placeholder with easy syntax
 
-var TextBoxAndButtonViewModel = TextBoxAndButtonViewModel || function (textBoxModel, buttonModel, child) {
+var TextBoxAndButtonViewModel = TextBoxAndButtonViewModel || function (textBoxModel, buttonModel, customValidationMethod, child) {
 	var _self = this;
 	var _textBoxModel = textBoxModel;
 	var _buttonModel = buttonModel;
+	var _customValidationMethod = customValidationMethod;
 	var _child = child;
 
 	_self.init = function (obj) {
 		obj.textBox = _self.textBoxViewModel || new TextBoxViewModel(_textBoxModel);
 		_buttonModel.canClickMethod = function () {
-			return !obj.textBox.isEmpty();
+		    return !obj.textBox.isEmpty() && (!_customValidationMethod || _customValidationMethod(obj));
 		};
 		obj.canClick = _self.canClick || ko.computed(function () {
 				return !_buttonModel.canClickMethod();
